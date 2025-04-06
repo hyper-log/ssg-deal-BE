6dde5e2fd9b1550abce71604764b93505b4cf858
jeonjuho <wjswngh23@daum.net>
2025-04-06 20:58:33 +0900

    feat: initialize msa project
    - 멀티 모듈 아키텍처를 채택하여 구성
    - 공통 모듈 사용

---
+++ cart_service/src/main/java/on/ssgdeal/cart_service/configuration/CommonConfig.java
@@ -0,0 +1,12 @@
+package on.ssgdeal.cart_service.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+    "on.ssgdeal.common"
+})
+public class CommonConfig {
+
+}
