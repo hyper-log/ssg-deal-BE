ceaa7ac1e3cfe9160fb700d41c7b6f029dd4322d
hyper <revoluer9@gmail.com>
2025-04-10 11:06:01 +0900

    fix: OrderService JPAAuditing Rollback

--- common/src/main/java/on/ssgdeal/common/jpa/JpaAuditingConfig.java
+++ common/src/main/java/on/ssgdeal/common/jpa/JpaAuditingConfig.java
@@ -3,6 +3,7 @@ package on.ssgdeal.common.jpa;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import on.ssgdeal.common.auth.passport.Passport;
 import on.ssgdeal.common.auth.passport.PassportUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,13 +21,11 @@ public class JpaAuditingConfig {
 
     @Bean
     public AuditorAware<Long> loginUserAuditorAware() {
-        return () -> Optional.of(408L);
-        /*
         return () -> {
             Passport passport = passportUtil.getPassportBy(httpServletRequest);
             return Optional.of(passport.getUserId());
         };
-         */
+
     }
 }
 
