b4418f546d370984cc521311abe9e9a4732dc93a
hyper <revoluer9@gmail.com>
2025-04-10 23:59:47 +0900

    fix: OrderService 외부 서비스 Feign Client 이름 원복

--- order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/user/feign/UserServiceFeignClient.java
+++ order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/user/feign/UserServiceFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user_service")
+@FeignClient(name = "user-service")
 public interface UserServiceFeignClient {
 
     @PostMapping("/internal/v1/users/validations/deliveries")
