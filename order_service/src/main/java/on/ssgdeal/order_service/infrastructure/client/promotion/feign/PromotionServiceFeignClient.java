70c1b64f13b62fd70e5787df835f3bd1667516a0
hyper <revoluer9@gmail.com>
2025-04-10 21:25:43 +0900

    fix: OrderService 외부 서비스 Feign Client 이름 변경

--- order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/promotion/feign/PromotionServiceFeignClient.java
+++ order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/promotion/feign/PromotionServiceFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "promotion-service")
+@FeignClient(name = "promotion_service")
 public interface PromotionServiceFeignClient {
 
     @PostMapping("/internal/v1/promotions/products/stocks/decrease/detail")
