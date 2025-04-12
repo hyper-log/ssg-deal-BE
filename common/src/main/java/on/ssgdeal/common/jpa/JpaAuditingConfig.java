779543cefe1293ba9437915079e385818c007b43
Bulgogi-Pizza <quitendexit@gmail.com>
2025-04-12 21:45:03 +0900

    fix: JpaAuditingConfig 테스트 파일로 올라간 것 수정, 원상복구

--- common/src/main/java/on/ssgdeal/common/jpa/JpaAuditingConfig.java
+++ common/src/main/java/on/ssgdeal/common/jpa/JpaAuditingConfig.java
@@ -1,7 +1,10 @@
 package on.ssgdeal.common.jpa;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import on.ssgdeal.common.auth.passport.Passport;
+import on.ssgdeal.common.auth.passport.PassportUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -12,45 +15,44 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @RequiredArgsConstructor
 public class JpaAuditingConfig {
 
-//    private final HttpServletRequest httpServletRequest;
-//    private final PassportUtil passportUtil;
-//
-//    private static boolean isCreateUserRequest(String requestUri, String requestMethod) {
-//        return requestUri.contains("/internal") &&
-//            requestUri.contains("/users") &&
-//            requestMethod.equals("POST");
-//    }
-//
-//    private static boolean isSignupAuthRequest(String requestUri, String requestMethod) {
-//        return requestUri.contains("/api") &&
-//            requestUri.contains("/auth/signup") &&
-//            requestMethod.equals("POST");
-//    }
-//
-//    private static boolean isLoginAuthRequest(String requestUri, String requestMethod) {
-//        return requestUri.contains("/api") &&
-//            requestUri.contains("/auth/login") &&
-//            requestMethod.equals("POST");
-//    }
+    private final HttpServletRequest httpServletRequest;
+    private final PassportUtil passportUtil;
+
+    private static boolean isCreateUserRequest(String requestUri, String requestMethod) {
+        return requestUri.contains("/internal") &&
+            requestUri.contains("/users") &&
+            requestMethod.equals("POST");
+    }
+
+    private static boolean isSignupAuthRequest(String requestUri, String requestMethod) {
+        return requestUri.contains("/api") &&
+            requestUri.contains("/auth/signup") &&
+            requestMethod.equals("POST");
+    }
+
+    private static boolean isLoginAuthRequest(String requestUri, String requestMethod) {
+        return requestUri.contains("/api") &&
+            requestUri.contains("/auth/login") &&
+            requestMethod.equals("POST");
+    }
 
     @Bean
     public AuditorAware<Long> loginUserAuditorAware() {
-        return Optional::empty;
-//        return () -> {
-//            String requestUri = httpServletRequest.getRequestURI();
-//            String requestMethod = httpServletRequest.getMethod();
-//
-//            if (
-//                isCreateUserRequest(requestUri, requestMethod) ||
-//                    isSignupAuthRequest(requestUri, requestMethod) ||
-//                    isLoginAuthRequest(requestUri, requestMethod)
-//            ) {
-//                return Optional.empty();
-//            }
-//
-//            Passport passport = passportUtil.getPassportBy(httpServletRequest);
-//            return Optional.of(passport.getUserId());
-//        };
+        return () -> {
+            String requestUri = httpServletRequest.getRequestURI();
+            String requestMethod = httpServletRequest.getMethod();
+
+            if (
+                isCreateUserRequest(requestUri, requestMethod) ||
+                    isSignupAuthRequest(requestUri, requestMethod) ||
+                    isLoginAuthRequest(requestUri, requestMethod)
+            ) {
+                return Optional.empty();
+            }
+
+            Passport passport = passportUtil.getPassportBy(httpServletRequest);
+            return Optional.of(passport.getUserId());
+        };
     }
 }
 
