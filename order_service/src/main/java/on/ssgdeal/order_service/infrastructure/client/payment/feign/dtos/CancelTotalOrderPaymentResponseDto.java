fecfc5c59faa8b79221888ef3b1f792b56fec87f
hyper <revoluer9@gmail.com>
2025-04-14 11:24:46 +0900

    feat: OrderService 외부 결제 취소 응답값 엔드포인트 스펙 변경에 따른 응답값 변경

--- order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/payment/feign/dtos/CancelTotalOrderPaymentResponseDto.java
+++ order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/payment/feign/dtos/CancelTotalOrderPaymentResponseDto.java
@@ -1,5 +1,9 @@
 package on.ssgdeal.order_service.infrastructure.client.payment.feign.dtos;
 
-public record CancelTotalOrderPaymentResponseDto(Long paymentId) {
+public record CancelTotalOrderPaymentResponseDto(String paymentKey,
+                                                 Long totalOrderId,
+                                                 Long totalAmount,
+                                                 String result
+) {
 
 }
