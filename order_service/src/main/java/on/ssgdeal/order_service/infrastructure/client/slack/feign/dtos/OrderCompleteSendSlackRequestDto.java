271bf65bb56ebf76bb86d7a4545d46970da59813
hyper <revoluer9@gmail.com>
2025-04-15 01:44:03 +0900

    fix: OrderService send Slack API DTO 스펙 변경

--- order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/slack/feign/dtos/OrderCompleteSendSlackRequestDto.java
+++ order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/slack/feign/dtos/OrderCompleteSendSlackRequestDto.java
@@ -6,16 +6,23 @@ import on.ssgdeal.order_service.domain.entity.dtos.UpdateTotalOrderSuccessDto;
 import on.ssgdeal.order_service.domain.enums.TotalOrderStatus;
 import on.ssgdeal.order_service.infrastructure.client.slack.dtos.TotalOrderCompleteSendInfoDto;
 
-public record OrderCompleteSendSlackRequestDto(Long totalOrderId, String slackEmail,
-                                               LocalDate OrderAt, Long paymentPrice,
+public record OrderCompleteSendSlackRequestDto(Long totalOrderId,
+                                               String slackEmail,
+                                               String ordererName,
+                                               LocalDate orderAt,
+                                               Long paymentPrice,
                                                TotalOrderStatus orderStatus) {
 
     public static OrderCompleteSendSlackRequestDto from(
         UpdateTotalOrderSuccessDto updateTotalOrderSuccessDto,
         TotalOrderCompleteSendInfoDto totalOrderCompleteSendInfoDto,
         LoginUserInfoDto loginUserInfoDto) {
-        return new OrderCompleteSendSlackRequestDto(updateTotalOrderSuccessDto.totalOrderId(),
-            loginUserInfoDto.slackEmail(), totalOrderCompleteSendInfoDto.orderAt(),
-            totalOrderCompleteSendInfoDto.paymentPrice(), TotalOrderStatus.EXPIRED);
+        return new OrderCompleteSendSlackRequestDto(
+            updateTotalOrderSuccessDto.totalOrderId(),
+            loginUserInfoDto.slackEmail(),
+            loginUserInfoDto.nickname(),
+            totalOrderCompleteSendInfoDto.orderAt(),
+            totalOrderCompleteSendInfoDto.paymentPrice(),
+            TotalOrderStatus.EXPIRED);
     }
 }
