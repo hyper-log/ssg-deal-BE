83646329437ea75b85061a6f2bb179f4b522fe57
jeonjuho <wjswngh23@daum.net>
2025-04-23 01:04:00 +0900

    feat: 프로듀서 재시도 횟수 설정

--- common/src/main/java/on/ssgdeal/common/messaging/config/KafkaProducerConfig.java
+++ common/src/main/java/on/ssgdeal/common/messaging/config/KafkaProducerConfig.java
@@ -55,6 +55,8 @@ public class KafkaProducerConfig {
         configs.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
         // 트랜잭션 타임아웃
         configs.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 300000);
+        // 재시도 횟수
+        configs.put(ProducerConfig.RETRIES_CONFIG, 3);
         // 동일 Producer 재시작 간에도 트랜잭션을 식별하기 위한 ID
         configs.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "tx-ssgdeal-" + applicationName);
 
