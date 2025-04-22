e2ac32478b2a4d59b2612372657b72afc8a3b12c
woo-lala <hyunj2034@naver.com>
2025-04-23 00:54:50 +0900

    feat: Common 모듈에 PageSortBy 메서드 추가 - QueryDsl에서 정렬 조건 찾을 때 사용하는 용도

--- common/src/main/java/on/ssgdeal/common/pageable/enums/PageSortBy.java
+++ common/src/main/java/on/ssgdeal/common/pageable/enums/PageSortBy.java
@@ -2,6 +2,7 @@ package on.ssgdeal.common.pageable.enums;
 
 import java.util.Arrays;
 import lombok.Getter;
+import on.ssgdeal.common.pageable.exception.PageableException;
 
 @Getter
 public enum PageSortBy {
@@ -18,5 +19,11 @@ public enum PageSortBy {
     public static boolean isValid(String sortBy) {
         return Arrays.stream(values())
             .anyMatch(e -> e.getSortBy().equals(sortBy));
+    }
+    public static PageSortBy from(String sortBy) {
+        return Arrays.stream(values())
+                .filter(e -> e.getSortBy().equals(sortBy))
+                .findFirst()
+                .orElseThrow(PageableException.InvalidSortByException::new);
     }
 }
