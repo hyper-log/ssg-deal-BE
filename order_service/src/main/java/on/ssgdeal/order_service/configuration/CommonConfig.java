e195328a5defdc92c2e057ea792656aaac3c3948
jeonjuho <wjswngh23@daum.net>
2025-04-28 10:48:07 +0900

    feat: 공통 모듈로 관리되는 OutboxRepository 와 Outbox Entity 스캔 추가

--- order_service/src/main/java/on/ssgdeal/order_service/configuration/CommonConfig.java
+++ order_service/src/main/java/on/ssgdeal/order_service/configuration/CommonConfig.java
@@ -3,13 +3,20 @@ package on.ssgdeal.order_service.configuration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan(basePackages = {
     "on.ssgdeal.common"
 })
-@EntityScan(basePackages = {"on.ssgdeal.order_service",
-    "on.ssgdeal.common.messaging.domain.entity"})
+@EntityScan(basePackages = {
+    "on.ssgdeal.order_service",
+    "on.ssgdeal.common.messaging.domain.entity"
+})
+@EnableJpaRepositories(basePackages = {
+    "on.ssgdeal.order_service",
+    "on.ssgdeal.common.messaging.domain.repository"
+})
 public class CommonConfig {
 
 }
