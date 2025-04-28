631323388ae47dc9caca1eae6d97d992bb09adc6
jeonjuho <wjswngh23@daum.net>
2025-04-28 19:52:04 +0900

    refactor(MessagingExceptionCode): 직렬화/역직렬화 예외 발생 시 HTTP 응답 값 변경

--- common/src/main/java/on/ssgdeal/common/messaging/exception/MessagingExceptionCode.java
+++ common/src/main/java/on/ssgdeal/common/messaging/exception/MessagingExceptionCode.java
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum MessagingExceptionCode implements ExceptionCode {
 
     NON_RECOVERABLE(HttpStatus.CONFLICT, "재시도 하지 않을 예외가 발생했습니다."),
-    EVENT_JSON_DESERIALIZE_EXCEPTION(HttpStatus.NOT_MODIFIED, "이벤트 역직렬화에 실패했습니다."),
-    EVENT_JSON_SERIALIZE_EXCEPTION(HttpStatus.NOT_MODIFIED, "이벤트 직렬화에 실패했습니다."),
+    EVENT_JSON_DESERIALIZE_EXCEPTION(HttpStatus.BAD_REQUEST, "이벤트 역직렬화에 실패했습니다."),
+    EVENT_JSON_SERIALIZE_EXCEPTION(HttpStatus.BAD_REQUEST, "이벤트 직렬화에 실패했습니다."),
     ;
 
     private final HttpStatus httpStatus;
